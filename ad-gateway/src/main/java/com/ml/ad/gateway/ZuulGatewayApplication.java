443c87c150cecd07b9a9024720b0c42c354c28a9
maolong <350723986@qq.com>
2021-11-08 19:40:00 +0800

    网关服务搭建

---
+++ ad-gateway/src/main/java/com/ml/ad/gateway/ZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.ml.ad.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * zuul启动类
+ *
+ * @author Mr.ml
+ * @date 2021/11/8
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
