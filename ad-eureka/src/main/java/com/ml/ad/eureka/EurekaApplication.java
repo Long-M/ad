1a24afcdc00aabd780aba5a03d9990f96ce72f5d
maolong <350723986@qq.com>
2021-11-08 18:41:05 +0800

    eureka服务搭建

---
+++ ad-eureka/src/main/java/com/ml/ad/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.ml.ad.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka启动类
+ *
+ * @author Mr.ml
+ * @date 2021/11/8
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
