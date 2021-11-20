ed3a6b4ad25d0aafcc3a40c52d3309f401b59a5e
maolong <350723986@qq.com>
2021-11-20 20:29:21 +0800

    关键词索引对象定义与服务实现

---
+++ ad-service/ad-search/src/main/java/com/ml/ad/util/CommonUtils.java
@@ -0,0 +1,50 @@
+package com.ml.ad.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * @author Mr.ml
+ * @date 2021/11/20
+ */
+@Slf4j
+public class CommonUtils {
+
+    public static <K, V> V getOrCreate(K key, Map<K, V> map, Supplier<V> factory) {
+        return map.computeIfAbsent(key, k -> factory.get());
+    }
+
+    public static String stringConcat(String... args) {
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    /**
+     * Tue Jan 01 08:00:00 CST 2021 转换为 date
+     *
+     * @param dateString 字符串
+     * @return date
+     */
+    public static Date parseStringDate(String dateString) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+            return DateUtils.addHours(dateFormat.parse(dateString), -8);
+        } catch (ParseException e) {
+            log.error("parseStringDate error {}", dateString);
+            return null;
+        }
+    }
+
+}
