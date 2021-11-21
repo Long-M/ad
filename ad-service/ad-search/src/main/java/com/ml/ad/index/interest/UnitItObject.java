bff68cae51ad83759cf118c5db22670125f704a6
maolong <350723986@qq.com>
2021-11-21 11:37:33 +0800

    兴趣索引对象定义与服务实现

---
+++ ad-service/ad-search/src/main/java/com/ml/ad/index/interest/UnitItObject.java
@@ -0,0 +1,19 @@
+package com.ml.ad.index.interest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Mr.ml
+ * @date 2021/11/21
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitItObject {
+
+    private Long unitId;
+    private String itTag;
+
+}
