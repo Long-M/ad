606b7048cf135115d83d08c9e611bfaf9521463b
maolong <350723986@qq.com>
2021-11-21 15:11:13 +0800

    第三层级索引操作实现

--- ad-service/ad-search/src/main/java/com/ml/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/ml/ad/handler/AdLevelDataHandler.java
@@ -1,14 +1,22 @@
 package com.ml.ad.handler;
 
+import com.alibaba.fastjson.JSON;
 import com.ml.ad.dump.table.AdCreativeTable;
+import com.ml.ad.dump.table.AdCreativeUnitTable;
 import com.ml.ad.dump.table.AdPlanTable;
+import com.ml.ad.dump.table.AdUnitTable;
 import com.ml.ad.index.DataTable;
 import com.ml.ad.index.IndexAware;
 import com.ml.ad.index.adplan.AdPlanIndex;
 import com.ml.ad.index.adplan.AdPlanObject;
+import com.ml.ad.index.adunit.AdUnitIndex;
+import com.ml.ad.index.adunit.AdUnitObject;
 import com.ml.ad.index.creative.CreativeIndex;
 import com.ml.ad.index.creative.CreativeObject;
+import com.ml.ad.index.creativeunit.CreativeUnitIndex;
+import com.ml.ad.index.creativeunit.CreativeUnitObject;
 import com.ml.ad.mysql.constant.OpType;
+import com.ml.ad.util.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -58,6 +66,57 @@ public class AdLevelDataHandler {
                 DataTable.of(CreativeIndex.class),
                 creativeObject.getAdId(),
                 creativeObject,
+                type
+        );
+    }
+
+    /**
+     * 广告单元
+     *
+     * @param unitTable 广告单元
+     * @param type      操作类型
+     */
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+        AdPlanObject adPlanObject = DataTable.of(AdPlanIndex.class).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}", unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject adUnitObject = new AdUnitObject(unitTable.getUnitId(), unitTable.getUnitStatus(), unitTable.getPositionType(), unitTable.getPlanId(), adPlanObject);
+        handleBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                adUnitObject.getUnitId(),
+                adUnitObject,
+                type);
+    }
+
+    /**
+     * 创意与广告单元关联关系
+     *
+     * @param creativeUnitTable 关联关系
+     * @param type              类型
+     */
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable, OpType type) {
+        if (type == OpType.UPDATE) {
+            log.error("CreateUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(CreativeIndex.class).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}", JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(creativeUnitTable.getAdId(), creativeUnitTable.getUnitId());
+
+        handleBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(creativeUnitObject.getAdId().toString(), creativeUnitObject.getUnitId().toString()),
+                creativeUnitObject,
                 type
         );
     }
