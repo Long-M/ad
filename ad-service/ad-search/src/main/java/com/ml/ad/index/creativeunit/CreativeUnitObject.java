5b14b9de23fbcf990c1700cfcd5feee2cbd870c2
maolong <350723986@qq.com>
2021-11-21 13:14:46 +0800

    创意与推广单元关联索引对象定义与服务实现

---
+++ ad-service/ad-search/src/main/java/com/ml/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,21 @@
+package com.ml.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Mr.ml
+ * @date 2021/11/21
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+    private Long unitId;
+
+    // adId-unitId
+
+}
