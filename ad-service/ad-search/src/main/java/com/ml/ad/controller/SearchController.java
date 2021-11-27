1dc932435659c3f4ffc1760a45c03405b91fe34e
maolong <350723986@qq.com>
2021-11-27 16:47:55 +0800

    完善广告检索服务

--- ad-service/ad-search/src/main/java/com/ml/ad/controller/SearchController.java
+++ ad-service/ad-search/src/main/java/com/ml/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.ml.ad.annotation.IgnoreResponseAdvice;
 import com.ml.ad.client.SponsorClient;
 import com.ml.ad.client.vo.AdPlan;
 import com.ml.ad.client.vo.AdPlanGetRequest;
+import com.ml.ad.search.ISearch;
+import com.ml.ad.search.vo.SearchRequest;
+import com.ml.ad.search.vo.SearchResponse;
 import com.ml.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,14 +26,23 @@ import java.util.List;
 @RestController
 public class SearchController {
 
+    private final ISearch search;
+
     private final RestTemplate restTemplate;
 
     private final SponsorClient sponsorClient;
 
     @Autowired
-    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient) {
+    public SearchController(ISearch search, RestTemplate restTemplate, SponsorClient sponsorClient) {
+        this.search = search;
         this.restTemplate = restTemplate;
         this.sponsorClient = sponsorClient;
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+        log.info("ad-search: fetchAds -> {}", JSON.toJSONString(request));
+        return search.fetchAds(request);
     }
 
     @IgnoreResponseAdvice
