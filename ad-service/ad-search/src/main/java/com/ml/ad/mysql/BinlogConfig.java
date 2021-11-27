080a95c115947fa137ee7ba09a433e6ed730b4f8
maolong <350723986@qq.com>
2021-11-27 14:51:51 +0800

    启动对Binlog的监听

---
+++ ad-service/ad-search/src/main/java/com/ml/ad/mysql/BinlogConfig.java
@@ -0,0 +1,28 @@
+package com.ml.ad.mysql;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mr.ml
+ * @date 2021/11/27
+ */
+@Component
+@ConfigurationProperties(prefix = "adconf.mysql")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BinlogConfig {
+
+    private String host;
+    private Integer port;
+    private String username;
+    private String password;
+
+    private String binlogName;
+    private Long position;
+
+}
---
+++ ad-service/ad-search/src/main/java/com/ml/ad/runner/BinlogRunner.java
@@ -0,0 +1,30 @@
+package com.ml.ad.runner;
+
+import com.ml.ad.mysql.BinlogClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mr.ml
+ * @date 2021/11/27
+ */
+@Slf4j
+@Component
+public class BinlogRunner implements CommandLineRunner {
+
+    private final BinlogClient client;
+
+    @Autowired
+    public BinlogRunner(BinlogClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        log.info("Coming in BinlogRunner ...");
+        client.connect();
+    }
+
+}
